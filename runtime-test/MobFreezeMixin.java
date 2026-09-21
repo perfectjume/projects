ea9111d9b97d557375fc0f65132232877ba5d9f4
perfectjume <senyume.senin@gmail.com>
2026-09-21 17:11:23 +0200

    Add high-priority Mob animation freeze

---
+++ runtime-test/MobFreezeMixin.java
@@ -0,0 +1,19 @@
+package com.misanthropy.hit_indicator.mixin.client;
+
+import com.misanthropy.hit_indicator.client.WindupTracker;
+import net.minecraft.world.entity.Mob;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = Mob.class, priority = 2000)
+public abstract class MobFreezeMixin {
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void hit_indicator$freezeMobWindupBeforeAnimationLayers(CallbackInfo ci) {
+        Mob self = (Mob) (Object) this;
+        if (self.level().isClientSide && WindupTracker.shouldFreeze(self)) {
+            ci.cancel();
+        }
+    }
+}
