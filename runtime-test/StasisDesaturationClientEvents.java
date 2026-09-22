0caebe3e58a69d91694d9bbfa4c42a7a3b52579c
perfectjume <senyume.senin@gmail.com>
2026-09-22 13:27:45 +0200

    Drop stencil allocation from offscreen grayscale path

--- runtime-test/StasisDesaturationClientEvents.java
+++ runtime-test/StasisDesaturationClientEvents.java
@@ -1,24 +1,17 @@
 package com.misanthropy.hit_indicator.client;
 
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import java.io.IOException;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterShadersEvent;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 
 @EventBusSubscriber(modid = "hit_indicator", value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public final class StasisDesaturationClientEvents {
     private StasisDesaturationClientEvents() {}
-
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> Minecraft.getInstance().getMainRenderTarget().enableStencil());
-    }
 
     @SubscribeEvent
     public static void onRegisterShaders(RegisterShadersEvent event) throws IOException {
